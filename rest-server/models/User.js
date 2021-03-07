2f35e8becb25cfd3d4316115fd032928c7af35e5
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-07 10:22:54 +0200

    Configurating mongoose user model

---
+++ rest-server/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const PASSWORD_VALIDATION_PATTERN = /^[a-zA-Z0-9]{6,}$/;
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+        validate: PASSWORD_VALIDATION_PATTERN
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
