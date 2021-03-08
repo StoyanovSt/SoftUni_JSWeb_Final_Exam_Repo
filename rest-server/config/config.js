006c0abb4723f0648bb8b6bea3340479cac55700
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-08 08:30:01 +0200

    Implementing login logic

--- rest-server/config/config.js
+++ rest-server/config/config.js
@@ -2,6 +2,7 @@ const config = {
     PORT: 5000,
     DB_CONNECTION: 'mongodb://localhost:27017/marketplace',
     SALT_ROUNDS: 9,
+    SECRET: 'public'
 };
 
 module.exports = config;
--- rest-server/routes.js
+++ rest-server/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
 const config = require('./config/config');
+const jwt = require('jsonwebtoken');
 
 // home page
 router.get('/', (req, res) => {
@@ -56,24 +57,75 @@ router.post('/register', (req, res) => {
                             })
                             .catch(error => {
                                 res.status(500).json({
-                                    message: error.message,
+                                    message: 'Internal server error!',
                                 });
                             });
                     })
                     .catch(error => {
                         res.status(500).json({
-                            message: error.message,
+                            message: 'Internal server error!',
                         });
                     });
             }
         })
         .catch(error => {
             res.status(500).json({
-                message: error.message,
+                message: 'Internal server error!',
             });
         });
-    
+
 });
 
+router.post('/login', (req, res) => {
+    // get user data
+    const userData = req.body;
+
+    // validate data
+    //.................................................
+
+    // check if such user exists in database
+    User.findOne({ username: userData.username })
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: 'User not found!',
+                });
+            }
+
+            // check if passwords match
+            bcrypt.compare(userData.password, user.password)
+                .then(response => {
+                    if (!response) {
+                        res.status(409).json({
+                            message: 'Username or password do not match the requirements!',
+                        });
+                    }
+
+                    // generate jwt and send it to the client as json
+                    const token = jwt.sign({
+                        _id: user._id,
+                        username: user.username
+                    }, config.SECRET);
+
+                    res.status(200).json({
+                        message: 'User is logged!',
+                        token: token,
+                    });
+
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        message: 'Internal server error!',
+                    });
+                });
+
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Internal server error!',
+            });
+        });
+
+});
 
 module.exports = router;
