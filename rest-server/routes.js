e45d639334559dff577270b2e253cd680ed48e7b
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-08 17:34:21 +0200

    Implementing details page logic

--- rest-server/routes.js
+++ rest-server/routes.js
@@ -253,4 +253,23 @@ router.post('/product/edit/:productId', (req, res) => {
         })
 });
 
+router.get('/product/details/:productId', (req, res) => {
+    // get product id
+    const productId = req.params.productId;
+
+    // get product by id from database
+    Article.findById(productId).lean()
+        .then(article => {
+            res.status(200).json({
+                article,
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Internal server error!',
+            });
+        });
+
+});
+
 module.exports = router;
