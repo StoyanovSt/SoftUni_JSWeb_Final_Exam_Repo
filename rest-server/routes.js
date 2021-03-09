7e0465251ba1e4e210eaca4b3f804c5c49d5f48c
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-09 10:18:06 +0200

    Implementing page not found logic

--- rest-server/routes.js
+++ rest-server/routes.js
@@ -302,4 +302,12 @@ router.get('/profile', (req, res) => {
 
 });
 
+//---------------------------------------
+// page not found
+router.get('*', (req, res) => {
+    res.status(404).json({
+        message: 'Page not found!'
+    });
+});
+
 module.exports = router;
