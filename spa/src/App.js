1d4d36b571ffae95d4c490c33294f8c8fc3acb86
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-13 14:08:24 +0200

    Creating basic components architecture

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,17 +1,17 @@
 
 import './App.css';
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
-import Home from './components/Home.js';
-import Register from './components/Register.js';
-import Login from './components/Login.js';
-import ProductDetails from './components/ProductDetails.js';
-import EditProduct from './components/EditProduct.js';
-import CreateProduct from './components/CreateProduct.js';
-import Contacts from './components/Contacts.js';
-import PageNotFound from './components/PageNotFound.js';
-import UserProfile from './components/UserProfile.js';
-import Notification from './components/Notification.js';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+import Home from './components/Home/Home.js';
+import Register from './components/Register/Register.js';
+import Login from './components/Login/Login.js';
+import ProductDetails from './components/ProductDetails/ProductDetails.js';
+import EditProduct from './components/EditProduct/EditProduct.js';
+import CreateProduct from './components/CreateProduct/CreateProduct.js';
+import Contacts from './components/Contacts/Contacts.js';
+import PageNotFound from './components/PageNotFound/PageNotFound.js';
+import UserProfile from './components/UserProfile/UserProfile.js';
+import Notification from './components/Notification/Notification.js';
 
 function App() {
   return (
