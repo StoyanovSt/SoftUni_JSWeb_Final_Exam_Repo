b259b27bd333d8d3927e63ca2dd266a094364dd1
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-15 11:15:52 +0200

    Doing some changes of contacts page css

--- spa/src/components/Contacts/Contacts.js
+++ spa/src/components/Contacts/Contacts.js
@@ -15,12 +15,12 @@ class Contacts extends React.Component {
                 <h2 id="site-info">Our online marketplace gives you the opportunity to register an account, to offer products and also to contact with owners of other products of which you are interested.</h2>
                 <img src="../../images/bee.png" id="bee-pic" alt="Bee" />
                 
-                <h3 id="additional-info-heading">You can find us on:</h3>
+                <h3 id="additional-info-heading"><strong>You can find us on:</strong></h3>
                 <br />
                 <div id="additional-info">
-                    <h4>Email: healthy_world@abv.bg</h4>
-                    <h4>Phone: 089* *** ***</h4>
-                    <h4>Company address: 5 Industrialna st., Opaka, Bulgaria</h4>                  
+                    <h4><strong>Email:</strong> healthy_world@abv.bg</h4>
+                    <h4><strong>Phone:</strong> 089* *** ***</h4>
+                    <h4><strong>Company address:</strong> 5 Industrialna st., Opaka, Bulgaria</h4>                  
                 </div>
             </Fragment>
         );
