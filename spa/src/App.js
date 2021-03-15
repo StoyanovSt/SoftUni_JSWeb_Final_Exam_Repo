995a18c6d0f2808490973e4df7fa97843de345bc
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-15 11:12:01 +0200

    Rebuilding contacts page component

--- spa/src/App.js
+++ spa/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="site-wrapper">
       <Header />
-      <PageNotFound />
+      <Contacts />
       <Footer />      
     </div>
   );
--- spa/src/components/Contacts/Contacts.js
+++ spa/src/components/Contacts/Contacts.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Fragment } from 'react';
+
+import './Contacts.css';
 
 class Contacts extends React.Component {
     // constructor(props) {
@@ -7,29 +10,19 @@ class Contacts extends React.Component {
 
     render() {
         return (
-            <div class="row bg-light">
-                <div class="col-md-12">
-                    <h1 class="text-center">Healthy world</h1>
-                    <br />
-                    <h2 class="text-center">We are pleased to encourage you to use our platform as  an online marketplace where you can register your own account, to store your products and of course to get in touch with sellers of other products.</h2>
-                    <br />
-                    <br />
-                    <br />
-                </div>
-
-
-                <div class="col-md-8">
-                    <h3>You can find us on:</h3>
-                    <br />
+            <Fragment>
+                <h1 id="company-name">Healthy world Ltd.</h1>
+                <h2 id="site-info">Our online marketplace gives you the opportunity to register an account, to offer products and also to contact with owners of other products of which you are interested.</h2>
+                <img src="../../images/bee.png" id="bee-pic" alt="Bee" />
+                
+                <h3 id="additional-info-heading">You can find us on:</h3>
+                <br />
+                <div id="additional-info">
                     <h4>Email: healthy_world@abv.bg</h4>
+                    <h4>Phone: 089* *** ***</h4>
+                    <h4>Company address: 5 Industrialna st., Opaka, Bulgaria</h4>                  
                 </div>
-                <div class="col-md-8">
-                    <h4>Phone: 089* *** *** </h4>
-                </div>
-                <div class="col-md-8">
-                    <h4>Address: ...</h4>
-                </div>
-            </div>
+            </Fragment>
         );
     }
 }
