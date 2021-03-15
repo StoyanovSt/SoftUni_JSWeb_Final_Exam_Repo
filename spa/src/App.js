d9c7714d8e27e7077d42666c1c69999ec9a30330
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-15 12:22:12 +0200

    Rebuilding product details page component

--- spa/src/App.js
+++ spa/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="site-wrapper">
       <Header />
-      <Contacts />
+      <ProductDetails />
       <Footer />      
     </div>
   );
--- spa/src/components/ProductDetails/ProductDetails.js
+++ spa/src/components/ProductDetails/ProductDetails.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Fragment } from 'react';
+
+import './ProductDetails.css';
 
 class ProductDetails extends React.Component {
     // constructor(props) {
@@ -7,25 +10,27 @@ class ProductDetails extends React.Component {
 
     render() {
         return (
-            <div className="row bg-light">
-                <div className="col-md-8">
-                    <h1>Пчелен мед</h1>
-                    <img className="img-thumbnail" src="../images/honey-jar.jpg" alt="Пчелен мед" />
+            <Fragment>
+                <h1 id="product-name">Honey</h1>
+                <div className="col-md-12">
+                    <img className="img-thumbnail" src="../images/honey-jar.jpg" id="product-pic" alt="Пчелен мед" />
                 </div>
-                <div className="col-md-12 text-center">
-                    <h3>Описание:</h3>
-                    <p>Продавам биологично чист пчелен мед. Година на производство - юни, 2021г. </p>
-                    <p>Цена: 10лв./кг </p>
-                    <p>Продавач: petko@abv.bg</p>
-                    <br />
 
-                    {/* if this.currentMovie.amITheCreator         */}
-                    <p>
-                        <a className="btn btn-dark" href="/product/edit">Edit</a>
+                <div className="col-md-12 text-center">
+                    <h3>Description:</h3>
+                    <p>Продавам биологично чист пчелен мед. Година на производство - юни, 2021г. </p>
+                    <p><strong>Price:</strong> 10 lv./kg </p>
+                    <p><strong>Seller:</strong> Петко Петков</p>
+                    <p><strong>Email:</strong>...</p>
+
+                    {/* if user is creator */}
+                    <p id="buttons">
+                        <a className="btn btn-secondary" href="/product/edit">Edit</a>
                         <a className="btn btn-danger" href="/product/delete">Delete</a>
+                        {/* <a className="btn btn-success" href="/product/buy">Buy product</a> */}
                     </p>
                 </div>
-            </div>
+            </Fragment>
         );
     }
 }
