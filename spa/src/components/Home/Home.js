d210bc73b369ed88751d59808bc80086f9c7477f
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-03-20 08:57:32 +0200

    Implementing functionality for getting all products from DB

--- spa/src/components/Home/Home.js
+++ spa/src/components/Home/Home.js
@@ -1,36 +1,54 @@
 import React from 'react';
 import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
 
+import Product from '../Product/Product.js';
 import './Home.css';
 
 class Home extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:5000/api/')
+            .then(res => res.json())
+            .then(allProducts => {
+                this.setState({ products: allProducts });
+            })
+            .catch(error => console.log(error));
+    }
 
     render() {
         return (
             <Fragment>
                 {/* if loggedIn */}
 
-                {/* <div className="products-container">
+                <div className="products-container">
                     <div className="row bg-light">
-                        
+                        {this.state.products.map(product => {
+                            <Product
+                                key={product._id}
+                                data={product}
+                            />
+                        })}
 
-                        <div className="col-md-12" id="empty-container">
+                        {/* <div className="col-md-12" id="empty-container">
                             <h3>No products so far...</h3>
-                        </div>
+                        </div> */}
                     </div>
-                </div> */}
+                </div>
 
                 {/* else */}
 
-                <div className="guest-home">
+                {/* <div className="guest-home">
                     <h1>Welcome to Healthy world!</h1>
                     <h2>The biggest online bee products marketplace</h2>
                     <img src="../../../images/sunflowers.jpg" className="rounded" id="guest-view" alt="Sunflowers" />
-                </div>
+                </div> */}
 
                 {/* if */}
             </Fragment>
--- spa/src/components/Product/Product.js
+++ spa/src/components/Product/Product.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 import './Product.css';
 
 class Product extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
             <div className="col-md-4">
-                <img src="../../../images/honey-jar.jpg" className="rounded-circle" alt="Honey" />
-                <p>Honey</p>
-                <p>Price: 10 lv/kg</p>
+                <img src={this.props.product.imageUrl} className="rounded-circle" alt="" />
+                <p>{this.props.product.product}</p>
+                <p>Price: {this.props.product.price} lv/kg</p>
                 <p>
-                    <Link to="/api/product/{productId}/details">
+                    <Link to={`/api/product/${this.props.product._id}/details`}>
                         <button type="button" className="btn btn-secondary">More info</button>
                     </Link>
                 </p>
